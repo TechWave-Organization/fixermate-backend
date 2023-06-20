4866bffa99bdb3a5bbf405e6d9325b3e8c691427
Faqu22 <facundoalvarez415@gmail.com>
2023-06-20 19:54:45 -0300

    Fix problems initials with models User and Person

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
-from urls import api
 
 
 api = NinjaAPI(
--- backend/src/user/models.py
+++ backend/src/user/models.py
@@ -1,10 +1,13 @@
 import uuid 
 from django.db import models
-
 from src.person.models import Person
 
 #Create your models here
-class user(models.Model):
+class User(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     password = models.CharField(max_length=100)
-    person_id = models.ForeignKey(Person.id, on_delete=models.CASCADE, null=True)
+    person_id = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = "User"
+        verbose_name_plural = "Users"
