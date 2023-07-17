beb1c7a37eb34702079e1441bd5ecded4f918b08
Nahuel Alvarez <66626569+NahuelAlvarez16@users.noreply.github.com>
2023-07-17 00:54:55 -0300

    Update models.py

--- src/phone/models.py
+++ src/phone/models.py
@@ -8,7 +8,7 @@ from src.phone.manager import PhoneManager
 
 class Phone(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    person_id = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, null=True)
     number_phone = models.PositiveIntegerField
     objects = PhoneManager()
     
