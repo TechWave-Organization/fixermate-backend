40ce398ced8f42789a46b7f443dc2a0d487ff945
Faqu22 <facundoalvarez415@gmail.com>
2023-07-17 11:02:59 -0300

    Add api routh clients

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
 from src.person.API.routes import router as persons_router
+from src.client.API.routes import router as clients_router
 
 api = NinjaAPI(
     title="Fixermate",
@@ -9,7 +10,7 @@ api = NinjaAPI(
     auth=None,
 )
 api.add_router("persons", persons_router)
-
+api.add_router("clients", clients_router)
 urlpatterns = [
     path('admin/', admin.site.urls), path('api/', api.urls)
 ]
