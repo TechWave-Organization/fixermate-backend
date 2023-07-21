96a9dda9954ef9448004bbb749cd6925b0d8704c
NahuelAlvarez16 <nahuel.alvarez161@hotmail.com>
2023-07-20 21:30:06 -0300

    Implement user route.

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from ninja import NinjaAPI
-from src.person.API.routes import router as persons_router
+from src.user.API.routes import router as users_router
 
 api = NinjaAPI(
     title="Fixermate",
     description="Backend of Fixermate",
     auth=None,
 )
-api.add_router("persons", persons_router)
+
+api.add_router("users", users_router)
 
 urlpatterns = [
-    path('admin/', admin.site.urls), path('api/', api.urls)
+    path('admin/', admin.site.urls), path('api/', api.urls),
 ]
