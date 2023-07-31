8f4c7c2df5dac2ee7a089b78adbbc78a578845b1
Faqu22 <facundoalvarez415@gmail.com>
2023-07-31 00:49:55 -0300

    Implementation of decorator 'permissions_required' and add aesthetic programmic

--- src/client/API/routes.py
+++ src/client/API/routes.py
@@ -1,13 +1,46 @@
 import uuid
 from ninja import Router
 from ninja.pagination import paginate
+from utils.decorators.permissions import permissions_required
 from utils.pagination import CustomPagination
 from src.schemas import client
 from src.client.models import Client
 from typing import Any, List
 
-
 router = Router(tags=["Clients"])
+
+
+@router.get(
+    "",
+    response={
+        200: List[client.OutClient],
+        400: Any,
+        401: Any,
+        403: Any,
+        404: Any,
+        500: dict,
+    },
+)
+@permissions_required(role="client", permissions=["view"])
+def list_client(request):
+    return Client.objects.get_list()
+
+
+@router.get(
+    "{client_id}",
+    response={
+        200: client.OutClient,
+        400: Any,
+        401: Any,
+        403: Any,
+        404: Any,
+        500: dict,
+    },
+)
+@permissions_required(role="client", permissions=["view"])
+def get_client(request, client_id: uuid.UUID):
+    return Client.objects.create_schema(Client.objects.get(client_id))
+
 
 @router.post(
     "",
@@ -20,23 +53,10 @@ router = Router(tags=["Clients"])
         500: dict,
     },
 )
+@permissions_required(role="client", permissions=["create"])
 def create_client(request, data: client.InClient):
     return Client.objects.create_client(data)
 
-
-@router.get(
-    "{client_id}",
-    response={
-        200: client.OutClient,
-        400: Any,
-        401: Any,
-        403: Any,
-        404: Any,
-        500: dict,
-    },
-)
-def get_client(request, client_id: uuid.UUID):
-    return Client.objects.get_client(client_id)
 
 @router.put(
     "{client_id}",
@@ -47,35 +67,24 @@ def get_client(request, client_id: uuid.UUID):
         403: Any,
         404: Any,
         500: dict,
-    }
+    },
 )
+@permissions_required(role="client", permissions=["update"])
 def update_client(request, client_id: uuid.UUID, data: client.InClient):
     return Client.objects.update_client(client_id, data)
 
-@router.get(
-    "",
-    response={
-        200: List[client.OutClient],
-        400: Any,
-        401: Any,
-        403: Any,
-        404: Any,
-        500: dict,
-    }
-)
-def list_client(request):
-    return Client.objects.all()
 
 @router.delete(
     "{client_id}",
     response={
-        204: Any,
+        200: Any,
         400: Any,
         401: Any,
         403: Any,
         404: Any,
-        500: dict,
-    }
+        500: Any,
+    },
 )
+@permissions_required(role="client", permissions=["delete"])
 def delete_client(request, client_id: uuid.UUID):
     return Client.objects.delete_client(client_id)
