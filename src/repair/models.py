5ee948b46b5385dfcc19783670f52d43585c884e
Faqu22 <facundoalvarez415@gmail.com>
2023-07-31 00:54:05 -0300

    Implementation of class inheritance BaseModel and ForeignKeys

--- src/repair/models.py
+++ src/repair/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 import uuid
+from src.client.models import Client
 from src.repair.manager import RepairManager, RepairStatusManager, ServiceManager
+from src.device.models import Device, DeviceModel
+from src.base.models import BaseModel
+
 # Create your models here.
 
-class Service(models.Model):
+class Service(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     price = models.IntegerField()
     name = models.CharField(max_length=100)
     description = models.CharField(max_length=450)
-    # device_model = models.ForeignKey("device_model", verbose_name="", on_delete=models.CASCADE)
+    device_model = models.ForeignKey(DeviceModel, verbose_name="", on_delete=models.CASCADE)
     objects = ServiceManager()
 
     class Meta:
@@ -16,10 +20,10 @@ class Service(models.Model):
         verbose_name_plural = "Services"
 
 
-class Repair(models.Model):
+class Repair(BaseModel):
     id  = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # client = models.ForeignKey("client", verbose_name=(""), on_delete=models.CASCADE) #Pending
-    # device = models.ForeignKey("device", verbose_name=(""), on_delete=models.CASCADE) #Pending
+    client = models.ForeignKey(Client, verbose_name=(""), on_delete=models.CASCADE) #Pending
+    device = models.ForeignKey(Device, verbose_name=(""), on_delete=models.CASCADE) #Pending
     service = models.ForeignKey(Service, verbose_name=(""), on_delete=models.CASCADE)
     objects = RepairManager()
 
@@ -28,7 +32,7 @@ class Repair(models.Model):
         verbose_name_plural = "Repairs"
 
 
-class RepairStatus(models.Model):
+class RepairStatus(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     status = models.CharField(max_length=100)
     repair = models.ForeignKey(Repair, verbose_name=(""), on_delete=models.CASCADE)
